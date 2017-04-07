ca03080e5f7d8b39578b756a34a45dd37786234d
Joel Salisbury <joel.salisbury@gmail.com>
2017-04-07 14:23:30 -0400

    first version

---
+++ sf.js
@@ -0,0 +1,18 @@
+Element.prototype.addEventListeners = function(events, f) {
+    for (event of events) {
+     this.addEventListener(event,f);
+    }
+};
+
+(function () {
+ var ls = document.querySelectorAll(".spamfree"); 
+ for (l of ls) {
+ 	l.addEventListeners(["touchstart","click"], function (){ 
+  	var n = this.getAttribute('data-sfname');
+    var f = this.getAttribute('data-sfdomain');
+    var f = "mailto:" + n + "@" + d;
+    this.setAttribute('href', full );
+  });
+ }
+  console.log("ran the spamfree link thingy");
+})();
